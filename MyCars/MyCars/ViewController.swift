8032b0903c7b348feecc84c104c4bcf31f3472c6
LolaChernysheva <lolita-chernysheva@mail.ru>
2021-03-07 13:37:59 +0300

    Добавлен метод, позволяющий загрузить данные из файла в CoreData и проверки наличия данных в Core Data
    Добавлен метод, позволяющий загрузить данные из файла в CoreData и проверки наличия данных в Core Data

--- MyCars/MyCars/ViewController.swift
+++ MyCars/MyCars/ViewController.swift
@@ -9,47 +9,91 @@
 import UIKit
 import CoreData
 
+
+
+
 class ViewController: UIViewController {
-  
-  //получаем Context
+    
+    //получаем Context
     //первый вариант получения контекста
-  //lazy var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-  //второй вариант получения контекста:
+    //lazy var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    //второй вариант получения контекста:
     
     var context: NSManagedObjectContext!
     
-  @IBOutlet weak var segmentedControl: UISegmentedControl!
-  @IBOutlet weak var markLabel: UILabel!
-  @IBOutlet weak var modelLabel: UILabel!
-  @IBOutlet weak var carImageView: UIImageView!
-  @IBOutlet weak var lastTimeStartedLabel: UILabel!
-  @IBOutlet weak var numberOfTripsLabel: UILabel!
-  @IBOutlet weak var ratingLabel: UILabel!
-  @IBOutlet weak var myChoiceImageView: UIImageView!
-  
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var markLabel: UILabel!
+    @IBOutlet weak var modelLabel: UILabel!
+    @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var lastTimeStartedLabel: UILabel!
+    @IBOutlet weak var numberOfTripsLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var myChoiceImageView: UIImageView!
     
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  
-  @IBAction func segmentedCtrlPressed(_ sender: UISegmentedControl) {
     
-  }
-  
-  @IBAction func startEnginePressed(_ sender: UIButton) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
     
-  }
-  
-  @IBAction func rateItPressed(_ sender: UIButton) {
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
-  }
+    func getDataFromFile() {
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "mark != nil")
+        var records = 0
+        do {
+            let count = try context.count(for: fetchRequest)
+            records = count
+            print("Is Data there already?")
+        } catch {
+            print(error.localizedDescription)
+        }
+        guard records == 0 else {return}
+        let pathToFile = Bundle.main.path(forResource: "data", ofType: "plist")
+        let dataArray = NSArray(contentsOfFile: pathToFile!)!
+        for dictionary in dataArray {
+            let entity = NSEntityDescription()
+            let car = NSManagedObject(entity: entity, insertInto: context) as! Car
+            let carDictionary = dictionary as! NSDictionary
+            car.mark = carDictionary["mark"] as? String
+            car.model = carDictionary["model"] as? String
+            car.rating = carDictionary["rating"] as? NSNumber
+            car.lastStarted = carDictionary["lastStarted"] as? Date
+            car.timesDriven = carDictionary["timesDriven"] as? NSNumber
+            car.myChoice = carDictionary["myChoice"] as? NSNumber
+            let imageName = carDictionary["imageName"] as? String
+            let image = UIImage(named: "imageName")
+            let imageData = image!.pngData()
+            car.imageData = imageData as Data?
+            let colorDictionary = carDictionary["tintColor"] as? NSDictionary
+            car.tintColor = getColor(colorDictionary: colorDictionary!)
+        }
+        
+    }
+    func getColor(colorDictionary: NSDictionary) -> UIColor {
+        let red = colorDictionary["red"] as! NSNumber
+        let green = colorDictionary["green"] as! NSNumber
+        let blue = colorDictionary["blue"] as! NSNumber
+        
+        return UIColor(red: CGFloat(red.floatValue)/255, green: CGFloat(green.floatValue)/255, blue: CGFloat(blue.floatValue)/255, alpha: 1.0)
+    }
+    
+    
+    @IBAction func segmentedCtrlPressed(_ sender: UISegmentedControl) {
+        
+    }
+    
+    @IBAction func startEnginePressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func rateItPressed(_ sender: UIButton) {
+        
+    }
 }
 
