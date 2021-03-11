b3ca7b0437b1c58423ff5c97f63c2ca1ee5bf157
LolaChernysheva <lolita-chernysheva@mail.ru>
2021-03-11 22:36:15 +0300

    Добавлен метод проверки наличия данных в базе и метод загрузки данных из файла
    Добавлен метод проверки наличия данных в базе и метод загрузки данных из файла

---
+++ MyCars/MyCars/Car+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Car+CoreDataClass.swift
+//  MyCars
+//
+//  Created by Лолита on 11.03.2021.
+//  Copyright © 2021 Ivan Akulov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Car)
+public class Car: NSManagedObject {
+
+}
--- MyCars/MyCars/ViewController.swift
+++ MyCars/MyCars/ViewController.swift
@@ -34,9 +34,65 @@ class ViewController: UIViewController {
         
     }
     
+    private func getDataFromFile() {
+        //проверка наличия записей в базе
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "mark != nil")
+        var records = 0
+        do {
+            records = try context.count(for: fetchRequest)
+            print("Is Data there already?")
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        guard records == 0 else { return }
+        
+        //получение пути к файлу, в котором хранятся данные
+        guard let pathToFile = Bundle.main.path(forResource: "data", ofType: "plist"),
+            //если удалось получить путь к файлу, то нужно извлечь оттуда массив
+            let dataArray = NSArray(contentsOfFile: pathToFile) else { return }
+        //итерация по массиву, получение данных и помещение этих данных в coreData в виде объектов
+        for dictionary in dataArray {
+            //создание объектов, в которые будут помещаться данные из массива
+            guard let entity = NSEntityDescription.entity(forEntityName: "Car", in: context) else { return }
+            let car = NSManagedObject(entity: entity, insertInto: context) as! Car
+            
+            //разворачивание словаря
+            let carDictionary = dictionary as! [String : AnyObject]
+            car.mark = carDictionary["mark"] as? String
+            car.model = carDictionary["model"] as? String
+            car.rating = carDictionary["rating"] as! Double
+            car.lastStarted = carDictionary["lastStarted"] as? Date
+            car.timesDriven = carDictionary["timesDriven"] as! Int16
+            car.myChoice = carDictionary["myChoice"] as! Bool
+
+            //получение названия изображения
+            if let imageName = carDictionary["imageName"] as? String,
+                //получение изображения
+                let image = UIImage(named: imageName) {
+                //преобразование изображения для хранения в виде Data
+                let imageData = image.pngData()
+                car.imageData = imageData
+            }
+            
+            if let colorDictionary = carDictionary["tintColor"] as? [String : Float] {
+                car.tintColor =  getColor(colorDictionary: colorDictionary)
+            }
+        }
+        
+    }
+    
+    private func getColor(colorDictionary: [String : Float]) -> UIColor {
+        guard let red = colorDictionary["red"],
+        let green = colorDictionary["green"],
+            let blue = colorDictionary["blue"] else { return UIColor() }
+        return UIColor(red: CGFloat(red / 255), green: CGFloat(green / 255), blue: CGFloat(blue / 255), alpha: 1.0)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+            getDataFromFile()
         
     }
     
